bf686ecaf35511c7b8f62cff13f575b9643cd934
Vinicius Eduardo <vinicius.eddu99@gmail.com>
2023-04-24 01:20:18 -0300

    Agora sim, refatorado, funcionando perfeitamente do GET, POST, PUT

--- src/app.js
+++ src/app.js
@@ -1,5 +1,60 @@
+const fs = require('fs').promises;
 const express = require('express');
+const { readPersonFiles, PERSON_PATH } = require('./fsUtils');
 
 const app = express();
+app.use(express.json());
+
+app.get('/pessoas/:id', async (req, res) => {
+    try {
+        const pessoas = await readPersonFiles();
+        const pessoa = pessoas.find(({ id }) => id === Number(req.params.id));
+        res.status(200).json(pessoa);
+    } catch (Error) {
+        res.status(500).send({ message: Error.message });
+    }
+});
+
+app.get('/pessoas', async (_req, res) => {
+    try {
+        const pessoas = await readPersonFiles();
+        res.status(200).json(pessoas);
+    } catch (Error) {
+        res.status(500).send({ message: Error.message });
+    }
+});
+
+app.post('/pessoas', async (req, res) => {
+    try {
+        const { name, age } = req.body;
+        const pessoas = await readPersonFiles();
+        const novaPessoa = {
+            id: pessoas[pessoas.length - 1].id + 1,
+            name,
+            age,
+        };
+        const allPeoples = JSON.stringify([...pessoas, novaPessoa]);
+        await fs.writeFile(PERSON_PATH, allPeoples);
+        res.status(200).json(novaPessoa);
+    } catch (Error) {
+        res.status(500).send({ message: Error.message });
+    }
+});
+
+app.put('/pessoas/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { name, age } = req.body;
+        const pessoas = await readPersonFiles();
+        const index = pessoas.findIndex((element) => element.id === Number(id));
+        pessoas[index] = { id: Number(id), name, age };
+
+        const pessoasAtualizadas = JSON.stringify(pessoas, null, 2);
+        await fs.writeFile(PERSON_PATH, pessoasAtualizadas);
+        res.status(200).json(pessoas[index]);
+    } catch (Error) {
+        res.status(500).send({ message: Error.message });
+    }
+});
 
 module.exports = app;
--- src/fsUtils.js
+++ src/fsUtils.js
@@ -1,39 +1,15 @@
-const fs = require('fs').promises;
+const fs = require('fs/promises');
 const path = require('path');
 
-const MOVIES_PATH = './movies.json';
+const PERSON_PATH = path.resolve(__dirname, './movies.json');
 
-const readFileMovies = async () => {
+const readPersonFiles = async () => {
     try {
-        const data = await fs.readFile(path.resolve(__dirname, MOVIES_PATH));
-        const movies = JSON.parse(data);
-        return movies;
-    } catch (err) {
-        console.error(`Arquivo não pôde ser lido: ${err}`);
+        const data = await fs.readFile(PERSON_PATH);
+        return JSON.parse(data);
+    } catch (Error) {
+        console.error(`Arquivo não pôde ser lido: ${Error}`);
     }
-    };
-    
-const writeFile = async (moviesArr) => {
-    await fs.writeFile(MOVIES_PATH, JSON.stringify(moviesArr));
 };
-
-async function updateMovieData(id, updatedMovieData) {
-    const oldMovies = await readFileMovies();
-    const filmesAtualizados = { id, ...updatedMovieData };
-    const updatedMovies = oldMovies.reduce((moviesList, currentMovie) => {
-        if (currentMovie.id === filmesAtualizados.id) return [...moviesList, filmesAtualizados];
-        return [...moviesList, currentMovie];
-    }, []);
-
-    const updatedData = JSON.stringify(updatedMovies);
-    try {
-        await writeFile(updatedData);
-        console.log(`Atualizou missão com o id ${id}`);
-
-        return filmesAtualizados;
-    } catch (err) {
-        console.error(`Erro na escrita do arquivo: ${err}`);
-    }
-}
-
-module.exports = { readFileMovies, writeFile, updateMovieData };
+ 
+module.exports = { readPersonFiles, PERSON_PATH };
